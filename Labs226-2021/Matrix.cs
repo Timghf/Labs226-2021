72b3132a4c939198744b7e2c5783a38a8fbe7f31
Timghf <90610665+Timghf@users.noreply.github.com>
2021-10-02 12:59:14 +0300

    Update Matrix.cs

--- Labs226-2021/Matrix.cs
+++ Labs226-2021/Matrix.cs
@@ -48,9 +48,7 @@ namespace Labs226_2021
         public static void MatrixTwo(int[,] array)
         {
             int a = 0;
-            
-
-            int[,] array = new int[8, 8];
+           
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
